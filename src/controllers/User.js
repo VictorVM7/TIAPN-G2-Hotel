2b242650baea3562aacb75207b803cd4bc9ae59b
Victor Magalhães De Souza <1266914@sga.pucminas.br>
2023-06-01 21:24:22 -0300

    CRUD User - 100% - Falta tratamentos e outras tabelas

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -12,57 +12,80 @@ exports.postUser = async (req, res) => {
             await user.create(req.body)
             res.status(200).json({ message: true })
         }
-    } catch (err) {
-        res.status(401).json({ message: false })
+    } catch (error) {
+        res.status(401).json({ message: error })
     }
 };
 
 // GET - Retorna todos os usuários cadastrados 
 exports.getUser = async (req, res) => {
     try {
-        const users = await user.findAll();
-        res.status(200).json({ users })
-    } catch (err) {
-        res.status(400).json({ err })
+        const usuario = await user.findAll();
+
+        if(!usuario){
+            res.status(400).json({ message: "Nenhum usuário encontrado" })
+        } else {
+            res.status(200).json({ usuario })
+        }
+        
+    } catch (error) {
+        res.status(400).json({ error })
     }
 };
 
 // GET - Retorna somente um usuário escolhido
 exports.getUser = async (req, res) => {
+    const { id } = req.params
+    const { name, email } = req.body
+
     try {
-        const users = await user.findOne();
-        res.status(200).json({ users })
-    } catch (err) {
-        res.status(400).json({ err })
+        const usuario = await user.findOne({ where: { name, email } })
+
+        if (!usuario) {
+            res.status(400).json({ message: "Usuário não encontrado!" })
+        } else {
+            res.status(200).json({ usuario })
+        }
+    } catch (error) {
+        res.status(400).json({ error })
     }
 };
 
 // DELETE - Deleta usuário
 exports.deleteUser = async (req, res) => {
     const { id } = req.params
-    const usuario = await user.findOne({ where: { id } })
 
-    if (!usuario) {
-        res.status(401).json({ message: "Usuário não encontrado!" })
-    }
-    else {
-        const usuario = await user.destroy({ where: { id } })
-        res.status(200).json({ ok: true })
+    try {
+        const usuario = await user.findOne({ where: { id } })
+
+        if (!usuario) {
+            res.status(401).json({ message: "Usuário não encontrado!" })
+        }
+        else {
+            const usuario = await user.destroy({ where: { id } })
+            res.status(200).json({ ok: true })
+        }
+    } catch (error) {
+        res.status(401).json({ message: error })
     }
 }
 
 // UPDATE - Altera algum dado do usuário
 exports.updateUser = async (req, res) => {
-    const {id} = req.params
-    const{name, email} = req.body
-    const usuario = await user.findOne({ where: { id } })
+    const { id } = req.params
+    const { name, email } = req.body
 
-    if(!user){
-        res.status(401).json({message:"Nenhum usuário encontrado"})
-    } else {
-        const usuario = await user.update({ name, email }, )
+    try {
+        const usuario = await user.findOne({ where: { id } })
+        
+        if (!user) {
+            res.status(401).json({ message: "Nenhum usuário encontrado" })
+        } else {
+            const usuario = await user.update({ name, email },)
+        }
+    } catch (error) {
+        res.status(401).json({ message: error })
     }
-
 }
 
 
