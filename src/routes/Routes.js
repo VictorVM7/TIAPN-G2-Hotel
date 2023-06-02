536a7e0f2aee0e50e6a8884028f533f0b363161f
Victor Magalhães De Souza <1266914@sga.pucminas.br>
2023-06-01 22:23:03 -0300

    CRUD User - 100% - Falta tratamentos e outras tabelas

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -11,3 +11,28 @@ router.put('/user-update/:id', UserController.updateUser)
 
 // Exportar
 module.exports = router;
+
+/*
+-- 3)
+select count(distinct(E.CodUsu))
+  from Emprestimo E
+ inner join Obra O
+    on O.CodObr = E.CodObr
+ where O.NomObr = 'Princípios de Sistemas de Banco de Dados Distribuído'
+   and E.DatDev is not null
+
+-- 4)
+select count(distinct(E.CodUsu))
+  from Emprestimo E
+ inner join Obra O
+    on O.CodObr = E.CodObr
+ where O.NomObr = 'Princípios de Sistemas de Banco de Dados Distribuído'
+   and exists (select EM.CodUsu
+			     from Emprestimo EM
+				inner join Obra OB
+			       on OB.CodObr = EM.CodObr
+				where OB.NomObr = 'Sistemas de Banco de Dados'
+				  and E.CodUsu = EM.CodUsu)
+
+
+*/ 
