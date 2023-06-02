2373abad40395e35141c3a629a88f710a031d8f0
Victor Magalhães De Souza <1266914@sga.pucminas.br>
2023-06-01 21:30:48 -0300

    CRUD User - 100% - Falta tratamentos e outras tabelas

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -7,6 +7,7 @@ const router = Router()
 router.get('/', UserController.getUser)
 router.post('/user-create', UserController.postUser)
 router.delete('/user-delete/:id', UserController.deleteUser)
+router.put('/user-update/:id', UserController.updateUser)
 
 // Exportar
 module.exports = router;
